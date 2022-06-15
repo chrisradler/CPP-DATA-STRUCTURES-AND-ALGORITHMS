93407630e90e04842e4d26c5193cfbc2104fd76c
Chris Radler <100222249+chrisradler@users.noreply.github.com>
2022-06-15 16:07:44 -0400

    Add files via upload

---
+++ nestedconditions.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x =45;
+    
+    if( x< 50){
+        
+        if(x < 25){
+            cout<<"x is less then 25 "<<endl;
+        }
+        else{
+            cout <<"x is less then 50"<<endl;
+            
+        }
+    }
+    else{
+        if( x< 75 ){
+            cout<<" x is less then 75 "<<endl;
+            
+        }
+        else{
+            cout <<"x is more than 75 "<<endl;
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    /*
+    Write a code that will ask the user to enter a score from 0-100
+    and then prints the letter grade according to below scheeme
+    if score is from (90 -100 ) then grade is A
+    
+    int grade ;
+    cout << "Enter your score" << endl;
+    cin >> grade ;
+    
+    if (grade >= 90) {
+        cout << "You got an A";
+    }
+    else  if (grade >= 80 && grade < 90) {
+        cout << "You got an B";
+    }
+    else  if (grade >= 70 && grade < 80) {
+        cout << "You got an C";
+    }
+    else  if (grade >= 60 && grade < 70) {
+        cout << "You got an D";
+    }
+    else {
+        cout << "You got an F";
+    }
+    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+  int day ;
+  cout << "Please enter the day in numbers 1-7: " << endl ; 
+ cin >> day ;
+ if (day == 1) {
+     cout << "It is monday";
+ }
+ else  if (day == 2) {
+     cout << "It is tueday";
+ }
+ else  if (day == 3) {
+     cout << "It is wednesday";
+ }
+ else  if (day == 4) {
+     cout << "It is thursday";
+ }else  if (day == 5) {
+     cout << "It is friday";
+ }
+ else  if (day == 6) {
+     cout << "It is saturday";
+ }
+ else  if (day == 7) {
+     cout << "It is sunday";
+ }
+ else {
+     cout << "Invalid response";
+ }
+ */
+    return 0;
+}
+
