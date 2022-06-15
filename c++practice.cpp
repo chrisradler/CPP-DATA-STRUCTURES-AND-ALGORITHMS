39f7416bf395631b1b60ab72a37ee69ba4b037d4
Chris Radler <100222249+chrisradler@users.noreply.github.com>
2022-06-15 16:04:55 -0400

    Add files via upload

---
+++ c++practice.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    
+    int num ;
+    int square ;
+    cout << "Please enter a number: " << endl;
+    cin >> num ;
+    
+    cout << "Find the square of the selected number: " << endl;
+    cin >> square ;
+    
+    if (square == num * num) {
+        cout << "It is the square" ;
+    }
+    else {
+        cout << "It is not the square" ;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    write code that will ask the user to enter the lenght of a rectangle and 
+    then height of it and then prints whether its a squre or not.
+    
+
+    
+    
+    
+    int length ;
+    int height ; 
+    cout << "Enter the length of the rectangle" << endl ; 
+    cin >> length ;
+    cout << "Enter the height of the rectangle" << endl ;
+    cin >> height ; 
+    if (length == height) {
+        cout << "It is a square";
+    }
+    else {
+        cout << "It is not a square";
+    }
+    */
+    /*
+    Write a  code that will aks the user to enter a id  and then password ,
+    if id is Chris and password is Goodluck its will print Welcome Chris otherwise it will print Invalid User
+    
+    
+    string userName ;
+    string password ;
+    
+    cout << "Please enter a username: " << endl;
+    cin >> userName ;
+    cout << "Enter a password: " << endl;
+    cin >> password ; 
+    if( userName == "Chris" && password == "Goodluck"){
+        cout << "Welcome Chris";
+    }
+    else {
+        cout << "Invalid User";
+    }
+    */
+    
+    /*
+    
+    Write a code that will ask the user to enter a year and then 
+    prints whether its a leap year or not .
+    a year is leap year if its a multiple of 4.
+    e.g 2020 , 2024 and 2016 was leap year.
+    
+    int year ;
+    cout << "Enter a year" << endl;
+    cin >> year ; 
+    if (year % 4 == 0){
+        cout << "It is a leap year";
+    }
+    else {
+        cout << "It is not a leap year";
+    }
+    
+    */
+    
+    
+    
+    
+    
+    
+    
+    /*
+    
+    Write a code that will ask the user to enter a number and then prints whether its a even or odd number.
+    a number is even if its a multiple of 2 otherwise its odd.
+    int num ;
+    cout << "Please choose a number: " << endl;
+    cin >> num ;
+    
+    if (num % 2 == 0) {
+        cout << "The number is even";
+    }
+    else {
+        cout << " The number is odd";
+    }
+    */
