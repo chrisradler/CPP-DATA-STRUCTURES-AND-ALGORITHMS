2ce887e58bcdabbafa4702d25ece779a2e359e8d
Chris Radler <100222249+chrisradler@users.noreply.github.com>
2022-09-20 15:47:42 -0400

    Update jaggedArrayPractice.cpp

--- jaggedArrayPractice.cpp
+++ jaggedArrayPractice.cpp
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <iostream>
 using namespace std ;
